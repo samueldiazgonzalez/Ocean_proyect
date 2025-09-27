f630271482c03a18b9b1db80da8c2ff87b85b128
Jeremy Jimenez <jeremyjimenez486@gmail.com>
2025-09-27 00:09:32 -0500

    Se hizo una prueba de registrar un cliente con exito a la base de datos

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "turismo";
+$dbname = "TurismoDB";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
--- register_process.php
+++ register_process.php
@@ -1,18 +1,21 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 include 'db.php';
 
 $nombre = $_POST['username'];
 $correo = $_POST['email'];
 $pass = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-$sql = "INSERT INTO Usuarios (nombre, correo, contraseña, notificaciones) 
-VALUES (?, ?, ?, 1)";
+$sql = "INSERT INTO Usuarios (nombre, correo, contrasena, notificaciones)
+        VALUES (?, ?, ?, 1)";
 
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("sss", $nombre, $correo, $pass);
 
 if ($stmt->execute()) {
-    echo "Registro exitoso.  href='login.html'>Iniciar sesión</a>";
+echo "Registro exitoso. <a href='login.html'>Iniciar sesión</a>";
 } else {
     echo "Error: " . $stmt->error;
 }
